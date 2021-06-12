aa0019715556628a6bd66d10d85d2b2c24dd194a
Israel Santizo <joseisraelss.jiss@gmail.com>
2021-06-12 08:32:19 -0600

    Se realiza codigo de conversion de texto a decimal

--- js/main.js
+++ js/main.js
@@ -1,36 +1,63 @@
-/*
-var text = prompt('Ingrese texto');
-function convertFromHex(hex) {
-    var hex = hex.toString();//force conversion
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
-
-function convertToHex(str) {
-    var hex = '';
-    for(var i=0;i<str.length;i++) {
-        hex += ''+str.charCodeAt(i).toString(16);
-    }
-    return hex;
-}
-*/
-function verificarTexto(){
-    
-}
 function convertirHex(){
+    //Elementos en HTML A  utilizar 
     var texto = document.getElementById('mensaje').value;
+    var table = document.getElementById('tableConvert');
+    var table2 = document.getElementById('tableResult'); 
+
     console.log(texto);
-    //var hex = text.toString();
+    //Arrays para alamacenasr codigo ASCII
+    // y conversion a binario
     var arr1 = [];
+    var arr2 =[];
+
+    //Conversion hexadecimal
 	for (var n = 0, l = texto.length; n < l; n ++) 
      {
 		var hex = Number(texto.charCodeAt(n)).toString(16);
+        var bin = Number(texto.charCodeAt(n)).toString(2);
 		arr1.push(hex);
+        arr2.push(bin);
         console.log(hex)
+        console.log(bin)
+        document.getElementById('resultado').innerHTML = arr1;       
 	 }
-	return arr1.join('');
-    document.getElementById('place').innerHTML = hex
+     
+     //Conversion binaria
+     for (var i = 0; i < arr1.length; i ++) 
+     {
+        //create new row
+        var newRow = table.insertRow(table.length);
+
+        for (let j = 1; j < arr1[i].length; j++) {
+
+             // create new cells
+            var cell = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+
+            // add values to the cells
+            cell.innerHTML = arr1[i];
+            cell2.innerHTML = arr2[i];
+        }
+	 }
+
+     for (var i = 0; i < arr1.length; i ++) 
+     {
+        //create new row
+        var newRow = table2.insertRow(table2.length);
+
+        for (let j = 1; j < arr1[i].length; j++) {
+
+             // create new cells
+            var cell = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+
+            // add values to the cells
+            cell.innerHTML = arr1[i];
+            cell2.innerHTML = arr2[i];
+        }
+	 }
+     
+     
+     return arr1.join("");
 }
 //console.log(convertirHex('Quien es primero.'));
